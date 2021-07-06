716ce7d7744344f4bb564b61fe2ab05117f4d3f1
David Akehurst <david.akehurst@itemis.de>
2021-07-06 20:50:59 +0200

    webpack example

---
+++ examples/js/webpack/src/worker.js
@@ -0,0 +1,4 @@
+import agl from '../node_modules/net.akehurst.language.editor-agl-editor-worker/net.akehurst.language.editor-agl-editor-worker.js'
+const AglWorker = agl.net.akehurst.language.editor.worker.AglWorker;
+
+var worker = new AglWorker();
